4373610a320d66b5773ccd8acb8ceddf0b03d3f7
KingSultanAbdelrahman <elgendyabdel@gmail.com>
2026-07-09 14:28:05 -0400

    yo

--- Gokart.py
+++ Gokart.py
@@ -4,70 +4,110 @@ def main():
     
     # Calculating front axle link angle 
     # Constant values for the front axle A, B: do not change.
- 
-    w = 10              # distance between front kingpins/top pivots 
-    s = 8              # Steering rack/ Tierod Length
-    #y = 3               # distance from front axle to steering rack in nuetral straight tires
+    
+
+    m = 300             # lbs
+    mew = 0.8            # static coefficient of friction
+    g = 32.174          # ft/s^2
+    h = 15/12              # vertical com
+
+    w = 40              # distance between front kingpins/top pivots 
+    winft = 40/12
+    s = 37              # Steering rack/ Tierod Length
+    y = 3               # distance from front axle to steering rack in nuetral straight tires
     l = 70              # length of back wheel to front wheel
+
 
     x = w - s
     strwheel = x/2                             # x distance in nuetral wheel position from kingpin to tie rod
-    r = x+sqrt(5**2)               # Steering arm link length (distance from kingpin to tie rod)
+    r = sqrt(strwheel**2 + y**2)               # Steering arm link length (distance from kingpin to tie rod)
           
-    print(f"r: {r:.1f}", end = " ")
+    #print(f"r: {r:.1f}", end = " ")
 
-    thetacenter = acos( strwheel / r)               # Left linkage angle for a straight wheel
-    phicenter = 180- degrees(acos( strwheel / r))   # Right linkage angle for a straight wheel
+    thetacenter = acos( strwheel / r)             # Left linkage angle for a straight wheel
+    phicenter = acos( strwheel / r)                # Right linkage angle for a straight wheel
+    
+
 
     i = 0.0             # increments through the distance between the kingpins (w-s) 
                         # to calculate the angles of the steering linkages and wheels
 
+    max_i = r - strwheel     #
+    while i < max_i :                       # calculates the angles of the steering linkages and wheels for a right turn
+        leftx = strwheel - i                # left kingpin to tie rod distance
+        rightx = strwheel + i               # right kingpin to tie rod distance
+        
+        leftlinkangle = acos (leftx / r)        # left linkage angle for a right turn
+        rightlinkangle = acos (rightx / r)       # right linkage angle for a right turn
+        
+        print(f"Rack shift i: {i:.2f} ")
+        print(f"Left Link Angle: {degrees(leftlinkangle):.1f} ", end = " ")
+        print(f"Right Link Angle: {degrees(rightlinkangle):.1f} ", end = " ")
+        
+        wheelleft = degrees(leftlinkangle - thetacenter)
+        wheelright = degrees(phicenter-rightlinkangle)
+        print(f"Wheel Left: {wheelleft:.1f} ", end = " ")
+        print(f"Wheel Right: {wheelright:.1f} ")
+
+        if wheelleft > 0 and wheelright > 0 and wheelleft < 30 and wheelright < 30:  # only calculate if the wheels are within a reasonable range
+            # For a right turn:
+            # right wheel = inside wheel
+            # left wheel = outside wheel
+            Rfromin = (l / tan(radians(abs(wheelright)))) + w/2    # Inner turning radius of the gokart in inches
+            Rfromout = (l / tan(radians(abs(wheelleft)))) - w/2      # Outer turning radius of the gokart in inches
+            RCOM = ((Rfromin + Rfromout) / 2)/12
+            print(f"Centerline Turn Radius from inner: {Rfromin/12:.1f} ft", end = " ")   #  convert to feet
+            print(f"Centerline Turn Radius from outer: {Rfromout/12:.1f} ft", end = " ")   # convert to feet 
+
+            #rtol = abs(Rin/l)
+            #print(f"Rin/l: {rtol:.1f} ")
+            ackermantheory = w/l
+            ackermanact = (1/tan(radians(abs(wheelleft)))) - (1/tan(radians(abs(wheelright))))
+            percenterror = ((ackermanact - ackermantheory) / ackermantheory) * 100
+            print(f"Ackerman Theory: {ackermantheory:.4f} ", end = " ")
+            print(f"Ackerman Actual: {ackermanact:.4f} ", end = " ")
+            print(f"Percent Error: {percenterror:.2f}%")  
+            
+            maxslidespeed = sqrt(mew*g*RCOM)
+            maxtipspeed = sqrt((g*winft*RCOM)/(2*h))
+            FOS = 1.25
+            allowablespeed = maxslidespeed/FOS
+            
+            print(f"Allowable speed with FOS = {FOS:.2f}: {allowablespeed:.1f} ft/s = {allowablespeed/1.46667:.1f} mph")
+            print(f"maxslidespeed: {maxslidespeed:.1f} ft/s = {maxslidespeed/1.46667:.1f} mph")
+            print(f"maxtipspeed: {maxtipspeed:.1f} ft/s = {maxtipspeed/1.46667:.1f} mph")
+            #maxslidespeed = sqrt(mew*g*RCOM)
+            #maxtipspeed = sqrt((g*w*RCOM)/(2*h))
+            
+
+            #for v in range(0, 100, 1): # mph
+           #     v = v * 1.46667 # convert to ft/s
+            #    slide = (v**2)/(g*RCOM)
+           #     tip = (m*h*v**2)/(g*RCOM)
+           #     print(f"Speed: {v/1.46667:.1f} mph, Slide: {slide:.2f} ft")
+
+                #if slide >= mew:
+                 #   print(f"slide detected at v: {v/1.46667:.1f} mph!")
+                  #  print("Turn radius COM:", RCOM)
+                   # print(f"Wheel Left: {wheelleft:.1f} ", end = " ")
+                    #print(f"Wheel Right: {wheelright:.1f} ")
+                    #break
+                #if tip >= mresist:
+                 #   print(f"tip detected at v: {v/1.46667:.1f} mph!") 
+                  #  print(f"Wheel Left: {wheelleft:.1f} ", end = " ")
+                   # print(f"Wheel Right: {wheelright:.1f} ")
+                    #break
+
+            if abs(percenterror) > 10:
+                print("Significant Ackerman error detected!")
+                print(f"percenterror: {percenterror:.1f}")
+             
+        i += 0.25
     
-    while i <= x :
-        print("x:", i, end = " ")
-        theta = acos (i / r)
-        #print("delta x: ", i, end = " ")
-        #print("theta: ", degrees(theta), end = " ")
-        
-        phi = acos ( (x-i) / r )
-        #print("phi:", degrees(phi))
-        
-        phiunmirr = 180-degrees(phi)
-
-        #print("phiunmirr:", phiunmirr, end = " ")
-        wheelleft = degrees(theta - thetacenter)
-        wheelright = phiunmirr - phicenter
-        print(f"wheelleft:{wheelleft:.1f} ", end = " ")
-        print(f"wheelright:{wheelright:.1f}")
-        i += 0.25   
-
     # at max angle of wheel, what is turn radius of the gokart?
-
-
-    Rout = (l / tan(radians(abs(wheelright)))) + w/2
-    Rin = (l / tan(radians(abs(wheelleft)))) - w/2
-   
-    print(f"Rin: {Rin:.1f} ", end = " ")
-    print(f"Rout: {Rout:.1f}", end = " ")
-
-    rtol = abs(Rin/l)
-    print(f"Rin/l: {rtol:.1f} ")
-    ackermantheory = w/l
-    print("cot out: ", 1/tan(radians(abs(wheelright))), end = " ")
-    print("cot in: ", 1/tan(radians(abs(wheelleft))), end = " ")
-    print("wheel right: ", wheelright, end = " ")
-    print("wheel left: ", wheelleft)
-
-    ackermanact = (1/tan(radians(abs(wheelright)))) - (1/tan(radians(abs(wheelleft))))
-    percenterror = ((ackermantheory - ackermanact) / ackermanact) * 100
-    print(f"ackermantheory: {ackermantheory:.4f} ", end = " ")
-    print(f"ackermanact: {ackermanact:.4f} ", end = " ")
-    print(f"percenterror: {percenterror:.2f}%")
+    ssf = (w/12)/(2*h) # static stability factor
+    print(f"Static Stability Factor: {ssf:.2f}")
     
-    A = 0 # never moves
-    B = w # never moves
-    C = A
-    D = s
     
 
 
--- acceleration.py
+++ acceleration.py
@@ -7,31 +7,32 @@ def main():
     
     gearratio = 5.5
     g = .85 # gear efficiency 
+    grav = 32.2 #
     m = 300 #lbs 
 
     RPM = ERPM / gearratio
     frictionconst = .03
-    friction = frictionconst * m
+    friction = -frictionconst * m
 
     force = (Etorque * gearratio * g)/(Wheeldiameter/2)               # force in lbf at wheel
 
     top_speed = (RPM * wheel_circumference )/60 # top speed in ft/sec
     print(f"Top Speed: {top_speed:.2f} ft/sec")  
     v0 = 0
-    a = ((force - friction)/m)*32.174
+    a = ((force + friction)/m)*32.174
     #i = 0
     t = 0
     v = 0
     dt = 0
     print("Topspeed in meters:", top_speed/3.28)
-    netforce = force - friction
+    netforce = force + friction
     while v < top_speed:                    # convert top speed to m/s
         v_mps = v * 0.3048
         #v = (v0 + a*dt) 
         #v = v * 3.28
 
-        forcedrag = (.5* .8 * 1.225 * (v_mps**2)* 1)/4.448          # convert to lbf after calculating in newtons
-        netforce = force - forcedrag - friction          #lbf
+        forcedrag = -(.5* .8 * 1.225 * (v_mps**2)* 1)/4.448          # convert to lbf after calculating in newtons
+        netforce = force + forcedrag + friction          #lbf
         a = ((netforce)/m)*32.174                        #ft/s^2  
                # 
         v0 = v_mps
@@ -42,6 +43,35 @@ def main():
         t += dt
         if v >= top_speed or netforce <= 0:
             break
+
+    # to stop in 3 secs, net force required, a = -14.3ft/s^2
+    print(v)
+    t = 4
+    netforce = -((v/t)*m)/grav
+    dt = 0
+    while v > 0:
+        v_mps = v * 0.3048
+        forcedrag = -(.5* .8 * 1.225 * (v_mps**2)* 1)/4.448   
+        forcebrake = -netforce + forcedrag + friction
+        a = ((netforce)/m)*32.174
+        print(f"Time: {t:.1f} s, Velocity: {v:.2f} ft/s, Acceleration: {a:.2f} ft/s^2, NetForce: {netforce:.2f} lbf, Drag: {forcedrag:.2f} lbf", f"BrakeForce: {forcebrake:.2f} lbf") 
+        v = v + a*dt
+        dt=.1
+        t -= dt
+        netforce = -((v/t)*m)/grav
+        if v < 0 or netforce >= 0:
+            break
+    print(f"Time: {t:.1f} s, Velocity: {v:.2f} ft/s, Acceleration: {a:.2f} ft/s^2, NetForce: {netforce:.2f} lbf, Drag: {forcedrag:.2f} lbf", f"BrakeForce: {forcebrake:.2f} lbf")
+
+    FOSBrake = 1.5
+    brakereq = FOSBrake * forcebrake
+    print(f"Required Brake Force = {brakereq:.2f} with Factor of Safety: {FOSBrake:.2f}")
+
+
+
+
+
+
 
 
 
